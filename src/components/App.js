1e64b970ba174a85a7d4380dd05b4e2e9113c2ff
sujith1806 <saisujithvalluru2@gmail.com>
2021-04-29 12:31:52 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,111 @@ import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts);
+    this.setState({account:accounts[0]})
+    // // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if(networkData) {
+      const decentragram = new web3.eth.Contract(Decentragram.abi, networkData.address)
+      // window.alert('Yay !Decentragram contract deployed to detected network.')
+      this.setState({ decentragram })
+      const imagesCount = await decentragram.methods.imageCount().call()
+      this.setState({ imagesCount })
+      // Load images
+      for (var i = 1; i <= imagesCount; i++) {
+        const image = await decentragram.methods.images(i).call()
+        this.setState({
+          images: [...this.state.images, image]
+        })
+      }
+      // Sort images. Show highest tipped images first
+      this.setState({
+        images: this.state.images.sort((a,b) => b.tipamount - a.tipamount )
+      })
+      this.setState({ loading: false})
+    } else {
+      window.alert('Decentragram contract not deployed to detected network.')
+    }
+  }
+
+  captureFile = event => {
+
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+  uploadImage = description => {
+    console.log("Submitting file to ipfs...")
+
+    //adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+      this.state.decentragram.methods.uploadImage(result[0].hash, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  tipImageOwner(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.decentragram.methods.tipImageOwner(id).send({ from: this.state.account, value: tipAmount }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
 
   constructor(props) {
     super(props)
     this.state = {
       account: '',
+      decentragram:null,
+      images:[],
+      loading:true
     }
+    this.uploadImage = this.uploadImage.bind(this)
+    this.tipImageOwner = this.tipImageOwner.bind(this)
+    this.captureFile = this.captureFile.bind(this)
   }
 
   render() {
@@ -23,10 +120,14 @@ class App extends Component {
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
+              captureFile={this.captureFile}
+              uploadImage={this.uploadImage}
+              images={this.state.images}
+              tipImageOwner={this.tipImageOwner}
             // Code...
             />
-          }
         }
+      
       </div>
     );
   }
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,64 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
 contract Decentragram {
   // Code goes here...
+  string public name = "Sujith";
+
+  uint public imageCount = 0;
+  mapping (uint => Image) public images;
+
+  struct Image{
+    uint id;
+    string hash;
+    string desc;
+    uint tipamount;
+    address payable author;
+  }
+
+  event ImageCreated(
+    uint id,
+    string hash,
+    string desc,
+    uint tipamount,
+    address payable author
+  );
+
+   event ImageTipped(
+    uint id,
+    string hash,
+    string desc,
+    uint tipamount,
+    address payable author
+   );
+  // _ is for local variables not state ones 
+  function uploadImage(string memory _imgHash,string memory _desc) public{
+    //validation checks
+    require(bytes(_desc).length>0);
+    require(bytes(_imgHash).length>0);
+    require(msg.sender != address(0x0));
+    imageCount++;
+
+    images[imageCount] = Image(imageCount,_imgHash,_desc,0,msg.sender);
+
+    emit ImageCreated(imageCount,_imgHash,_desc,0,msg.sender);
+  }  
+
+  //Store Images
+
+  //Create Images
+
+  //Tip Images
+  function tipImageOwner(uint _id) public payable{
+    Image memory _image = images[_id];
+    address payable _author = _image.author;
+    //fetch author
+    address(_author).transfer(msg.value);
+    //update the total tip amount of the image
+    _image.tipamount = _image.tipamount + msg.value;
+    //upload back to blockchain
+    images[_id] = _image;
+    
+    emit ImageTipped(_id,_image.hash,_image.desc,_image.tipamount,_author);
+
+  }
 }
